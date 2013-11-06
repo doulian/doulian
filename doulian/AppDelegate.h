87e8b00ac5b48fd403632a56d60755cf52c12bcb
Binson Zhang <bin183cs@gmail.com>
2013-11-06 08:48:00 +0800

    Initial Commit

---
+++ doulian/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  doulian
+//
+//  Created by bin3 on 13-11-6.
+//  Copyright (c) 2013年 feelapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
